15931b4fbd48378316b695c9e5fa638f25284afd
robaiat181 <fardinrobaiat@gmail.com>
2025-02-22 00:42:46 +0600

    Push My 2nd Module

---
+++ Module-2/Arithematic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a=10,b=20;
+    int sum=a+b;
+    printf("%d",sum);
+    return 0;
+}
+
+
+
+#include<stdio.h>
+
+int main()
+{
+    int x=20,y=30;
+    int minus=x-y;
+    printf("%d",minus);
+    return 0;
+}
---
+++ Module-2/conditional1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int taka;
+    scanf("%d",&taka);
+    if(taka==100)
+    {
+        printf("burger khabo");
+    }
+    else
+    {
+        printf("saradin Kicu khabona");
+    }
+    return 0;
+
+}
